9a16a3c5f56f20ee629991b9a1eab0969c025a23
Glenw00d <sarkadibalint20@gmail.com>
2025-06-06 11:05:38 +0200

    Update route.js

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,1 +1,31 @@
 
+export async function GET() {
+  const baseUrl = 'https://caffeinated-sarcasm.vercel.app';
+
+  const staticPages = [
+    '',                  // főoldal
+    'redbubble',         // redbubble oldal
+    'teepublic',         // teepublic oldal
+    'etsy',              // etsy oldal
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticPages
+    .map(
+      (page) => `
+    <url>
+      <loc>${baseUrl}/${page}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>`
+    )
+    .join('')}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
