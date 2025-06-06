165c609fbd2ce7741a83c9e6af3fb8f4cc0da941
Glenw00d <sarkadibalint20@gmail.com>
2025-06-06 10:59:20 +0200

    Update sitemap.xml.js

--- app/sitemap.xml.js
+++ app/sitemap.xml.js
@@ -2,10 +2,7 @@ export async function GET() {
   const baseUrl = 'https://caffeinated-sarcasm.vercel.app';
 
   const staticPages = [
-    '', // Főoldal
-  ];
-
-  const externalLinks = [
+    '',
     'https://www.redbubble.com/people/Sarkadib/shop?asc=u&ref=account-nav-dropdown',
     'https://www.teepublic.com/user/caffeinated-sarcasm-co',
     'https://www.etsy.com/shop/CaffeinatedSarcasmCo',
@@ -14,25 +11,13 @@ export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${staticPages
-    .map(
-      (page) => `
+    .map((page) => `
     <url>
-      <loc>${baseUrl}/${page}</loc>
+      <loc>${page}</loc>
       <changefreq>weekly</changefreq>
-      <priority>1.0</priority>
-    </url>`
-    )
-    .join('')}
-
-  ${externalLinks
-    .map(
-      (link) => `
-    <url>
-      <loc>${link}</loc>
-      <changefreq>monthly</changefreq>
       <priority>0.8</priority>
-    </url>`
-    )
+    </url>
+  `)
     .join('')}
 </urlset>`;
 
