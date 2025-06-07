ddc4e9338cf482041d3638de8687393bbb0ce213
Glenw00d <sarkadibalint20@gmail.com>
2025-06-07 15:30:35 +0200

    Create page.jsx

---
+++ app/designs/page.jsx
@@ -0,0 +1,8 @@
+export default function DesignsPage() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold mb-4">My Designs</h1>
+      <p className="text-lg">This is where your uploaded designs will be displayed.</p>
+    </div>
+  );
+}
